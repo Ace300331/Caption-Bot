5d3d26d1e5431ce0a780768c926640b015a6cd13
Ace300331 <kujoora923@gmail.com>
2025-06-26 19:25:41 +0530

    Update config.py

--- config.py
+++ config.py
@@ -6,6 +6,6 @@ class Config(object):
       BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "7295637877:AAGgcbpmQ03Q22LuLKlWl5EEG_BaUrU8XSU")
       API_ID = int(os.environ.get("APP_ID", "23790461"))
       API_HASH = os.environ.get("API_HASH", "2d0a7fb85f06246e93948bf7afc05fb3")
-      CAPTION_TEXT = os.environ.get("CAPTION_TEXT", "")
+      CAPTION_TEXT = os.environ.get("CAPTION_TEXT", "✔️ Join Us [@Ace_Files]")
       CAPTION_POSITION = os.environ.get("CAPTION_POSITION", "nil")
       ADMIN_USERNAME = os.environ.get("ADMIN_USERNAME", "Amit_Shadow")
